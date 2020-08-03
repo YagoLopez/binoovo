d28cf423a447271bb0bc901a102c69ba583aff75
Yago Lopez <yago.lopez@gmail.com>
2020-08-03 20:33:16 +0200

    Add styles.module.css

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import styles from '../public/styles.module.css'
 
 export const NoResults = ({ message }) => {
   return (
-    <>
+    <div className={styles.centerHorVer}>
       <p>{message}</p>
-      <p><Link href="/"><a>⇦ Back to Home Page</a></Link></p>
-    </>
+      <div><Link href="/"><a>⇦ Back to Home Page</a></Link></div>
+    </div>
   )
 }
