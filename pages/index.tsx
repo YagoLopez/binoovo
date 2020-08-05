c9f5de99e07963af6e548c9a964761ff915245b4
Yago Lopez <yago.lopez@gmail.com>
2020-08-06 01:07:54 +0200

    [Chore] Refactor "styles" as "css" 🛠

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import { LinearProgress } from '@rmwc/linear-progress'
 import { Typography } from '@rmwc/typography'
 import { TextField } from '@rmwc/textfield'
 import { Button } from '@rmwc/button'
-import styles from '../public/styles.module.css'
+import css from '../public/styles.module.css'
 
 export default () => {
 
@@ -28,9 +28,9 @@ export default () => {
     <form onSubmit={onSearchMovie}>
       <TopBar title={CONST.TITLE} />
       { isLoading && <LinearProgress /> }
-      <section className={styles.centerHor}>
+      <section className={css.centerHor}>
         <label>
-          <div className={styles.blockText}>
+          <div className={css.blockText}>
             <div><Typography use="headline6">🎞️ Search Movie</Typography></div>
             <div><Typography use="body1">For example: "Contact"</Typography></div>
           </div>
@@ -41,16 +41,16 @@ export default () => {
             label={CONST.SEARCH_MOVIE_PLACEHOLDER}
             trailingIcon={{icon: 'close', tabIndex: 0, onClick: () => setSearchterm('')}}
           />
-          <div className={styles.submitBtn}>
+          <div className={css.submitBtn}>
             <Button label="Search" theme={['secondaryBg', 'onSecondary']} raised type="submit"/>
           </div>
         </label>
-        <div className={styles.blockText}>
+        <div className={css.blockText}>
           <div>
             <Typography use="caption">This app uses "The Movie Database GraphQL API":</Typography>
           </div>
           <div>
-            <a href="https://tmdb-graphql.com/" target="_blank" className={styles.link} rel="noopener">
+            <a href="https://tmdb-graphql.com/" target="_blank" className={css.link} rel="noopener">
               https://tmdb-graphql.com
             </a>
           </div>
