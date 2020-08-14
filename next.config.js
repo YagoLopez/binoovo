4b98d0f65aaba9b43a081c94967e4529e9fc47f5
Yago Lopez <yago.lopez@gmail.com>
2020-08-14 22:02:22 +0200

    Fix typo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa')
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
-// If prod build then create PWA infrastructure
+// If is prod build then create PWA infrastructure
 module.exports = (phase, { defaultConfig }) => {
 
   if (phase === PHASE_PRODUCTION_BUILD) {
