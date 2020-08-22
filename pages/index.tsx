770eddf6052b8e25e02ce617948c6886f66617e4
Yago Lopez <yago.lopez@gmail.com>
2020-08-22 22:27:22 +0200

    Add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { useRouter } from 'next/router'
 import { TopBar } from '../components/TopBar'
-import { CONST } from '../constants';
+import { CONST } from '../constants'
 import { LinearProgress } from '@rmwc/linear-progress'
 import { Typography } from '@rmwc/typography'
 import { TextField } from '@rmwc/textfield'
 import { Button } from '@rmwc/button'
 import { Fab } from '@rmwc/fab'
-import { openExternalLink } from "../services/movie.service";
+import { openExternalLink } from '../services/movie.service'
 import css from '../public/styles.module.css'
 import PageHead from '../components/PageHead'
 
-
 const Index = () => {
-
   const router = useRouter()
   const [searchterm, setSearchterm] = useState('')
   const [isLoading, setIsLoading] = useState(false)
@@ -31,37 +29,68 @@ const Index = () => {
     <>
       <PageHead title={CONST.TITLE_INDEX} />
       <TopBar title={CONST.TITLE_INDEX} />
-      { isLoading && <LinearProgress /> }
+      {isLoading && <LinearProgress />}
       <form onSubmit={onSearchMovie} className={css.form}>
         <main className={css.centerHor}>
           <label>
             <div className={css.blockText}>
-              <div><Typography use="headline6">🎞️ Search Movie</Typography></div>
-              <div><Typography use="body1">For example: "Contact"</Typography></div>
+              <div>
+                <Typography use="headline6">🎞️ Search Movie</Typography>
+              </div>
+              <div>
+                <Typography use="body1">For example: "Contact"</Typography>
+              </div>
             </div>
             <TextField
               icon="search"
               value={searchterm}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchterm(e.target.value)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setSearchterm(e.target.value)
+              }
               label={CONST.INPUT_SEARCH_PLACEHOLDER}
-              trailingIcon={{icon: 'close', tabIndex: 0, onClick: () => setSearchterm('')}}
+              trailingIcon={{
+                icon: 'close',
+                tabIndex: 0,
+                onClick: () => setSearchterm(''),
+              }}
             />
             <div className={css.submitBtn}>
-              <Button label="Search" theme={['secondaryBg', 'onSecondary']} raised type="submit"/>
+              <Button
+                label="Search"
+                theme={['secondaryBg', 'onSecondary']}
+                raised
+                type="submit"
+              />
             </div>
           </label>
           <div className={css.blockText}>
-            <div><Typography use="caption">This app uses "The Movie Database GraphQL API":</Typography></div>
             <div>
-              <a href="https://tmdb-graphql.com/" target="_blank" className={css.link} rel="noopener">
+              <Typography use="caption">
+                This app uses "The Movie Database GraphQL API":
+              </Typography>
+            </div>
+            <div>
+              <a
+                href="https://tmdb-graphql.com/"
+                target="_blank"
+                className={css.link}
+                rel="noopener"
+              >
                 https://tmdb-graphql.com
               </a>
             </div>
             <p>
               <Typography use="caption">Developed by Yago López</Typography>
             </p>
-            <div className={css.fabBtn} onClick={() => openExternalLink(CONST.LIGHTHOUSE_AUDIT_URL)}>
-              <Fab icon="assessment" theme={['primaryBg', 'onPrimary']} title="Audit Report"/>
+            <div
+              className={css.fabBtn}
+              onClick={() => openExternalLink(CONST.LIGHTHOUSE_AUDIT_URL)}
+            >
+              <Fab
+                icon="assessment"
+                theme={['primaryBg', 'onPrimary']}
+                title="Audit Report"
+              />
               <div className={css.fabBtnText}>Audit</div>
             </div>
           </div>
