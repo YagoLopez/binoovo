1e85eee8894c2f09b2d6c0dc108ba81bda71c49f
Yago Lopez <yago.lopez@gmail.com>
2020-09-06 15:06:04 +0200

    Update nextjs and refactor cypress tests and config
    And add mdc class to body

--- components/movies-search/MovieList.tsx
+++ components/movies-search/MovieList.tsx
@@ -5,12 +5,11 @@ export const MovieList = ({ listData, imageData }) =>
   listData.map((movie, index) => (
     <List twoLine avatarList key={index}>
       <ListGroup>
-        <MovieListItem
-          movieData={movie}
-          baseUrl={imageData.baseUrl}
-          imgSize={imageData.posterSizes[0]}
-        />
+          <MovieListItem
+            movieData={movie}
+            baseUrl={imageData.baseUrl}
+            imgSize={imageData.posterSizes[0]}
+          />
       </ListGroup>
     </List>
-    )
-  )
+  ))
