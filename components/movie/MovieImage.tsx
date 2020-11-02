a13810fe030536437bd4e8222053f2c3bc4d3f1b
Yago López <yago.lopez@gmail.com>
2020-11-03 00:05:14 +0100

    use-next/image-in-search-results-list (#82)
    * Use next/image component in search results list
    
    * Remove third party lazy-load-images lib and refs
    
    * Reformat MovieImage.tsx with prettier

--- components/movie/MovieImage.tsx
+++ components/movie/MovieImage.tsx
@@ -3,19 +3,27 @@ import { getImageUrl, onClickMovieDetail } from '../../services/movie.service'
 
 interface MovieImgProps {
   data: {
-    movieId: string,
-    title: string,
-    posterPath: string,
-    baseUrl: string,
+    movieId: string
+    title: string
+    posterPath: string
+    baseUrl: string
     posterSizes: string[]
   }
 }
 
-export const MovieImage = ({ data: {movieId, title, baseUrl, posterPath, posterSizes} }: MovieImgProps) => (
-  posterPath &&
+export const MovieImage = ({
+  data: { movieId, title, baseUrl, posterPath, posterSizes },
+}: MovieImgProps) =>
+  posterPath && (
     <CardMedia
       square
       onClick={() => onClickMovieDetail(movieId, title)}
-      style={{backgroundImage: `url(${getImageUrl(baseUrl, posterSizes[2], posterPath)})`}}
+      style={{
+        backgroundImage: `url(${getImageUrl(
+          baseUrl,
+          posterSizes[2],
+          posterPath
+        )})`,
+      }}
     />
-)
+  )
--- next.config.js
+++ next.config.js
@@ -3,13 +3,18 @@ const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
 // If compile phase is prod build then create PWA infrastructure
 module.exports = (phase) => {
-
   if (phase === PHASE_PRODUCTION_BUILD) {
     return withPWA({
-      pwa: { dest: 'public' }
+      pwa: { dest: 'public' },
+      images: {
+        domains: ['image.tmdb.org'],
+      },
     })
   }
   return {
-    env: []
+    env: [],
+    images: {
+      domains: ['image.tmdb.org'],
+    },
   }
 }
